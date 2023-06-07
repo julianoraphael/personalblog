f62782843b1abe594d7b2ae67dda018e188ed1b3
Juliano Raphael Simões de Souza <julianoraphael@gmail.com>
2023-06-07 09:38:42 -0300

    Adding the logout link

--- personalblog/urls.py
+++ personalblog/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('posts/<int:id>/', detail_post, name='detail_post'),
     path('posts/create/', create_post, name='create_post'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='list_posts'), name='logout'),
 ]
